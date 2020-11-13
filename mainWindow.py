169cd2b8e4426ed7fbd8398f01ef658f1a1120da
xx <1347772729@qq.com>
2020-11-13 16:07:24 +0800

    first

--- mainWindow.py
+++ mainWindow.py
@@ -1,20 +1,23 @@
-from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QMessageBox
 import sys
 
-from PyQt5.uic.Compiler.qtproxies import QtGui
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 
-class ChattingWindow(QWidget):
-    def __init__(self):
-        super(ChattingWindow, self).__init__()
-        self.initUi()
-    
-    def initUi(self) -> None:
-        pass
-    
-    
-    
-    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+class ChatTingWindow(QTabWidget):
+    def __init__(self, parent=None):
+        super(ChatTingWindow, self).__init__(parent)
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tab3 = QWidget()
+        self.addTab(self.tab1, "消息")
+        self.addTab(self.tab2, '联系人分组')
+        self.addTab(self.tab3, '联系人索引')
+        self.show()
+
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:  # 关闭对话框
         reply = QMessageBox(QMessageBox.Question, self.tr('退出'), self.tr('您确定要退出吗'), QMessageBox.NoButton, self)
         yes = reply.addButton(self.tr('确定'), QMessageBox.YesRole)
         no = reply.addButton(self.tr('取消'), QMessageBox.NoRole)
@@ -23,3 +26,8 @@ class ChattingWindow(QWidget):
             a0.accept()
         else:
             a0.ignore()
+
+
+app = QApplication(sys.argv)
+mytab = ChatTingWindow()
+sys.exit(app.exec_())
